63ba3620ec237bc32aedc37abd00ad3fb6a3db53
Laxman Lingampally <laxmanl@virtusa.com>
2020-06-25 16:20:05 +0530

    code change for new core angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+//import { AppComponent } from './../../../hellong/src/app/app.component';
+import { Component } from "@angular/core"
+
+@Component({
+    selector:"my-app",
+    templateUrl:"./app.component.html",
+    styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+
+}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+//import { AppModule } from './../../counter-app/src/app/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import {} from "@angular/platformBrowserDynamic"
+
+import { AppModule } from "./app/app.module"
+
+
+platformBrowserDynamic().bootstrapModule(AppModule);
